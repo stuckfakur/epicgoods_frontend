60a7338d22e47da7754aad1fb59683adb6b5d608
benaleobayu <benaleobs@gmail.com>
2024-08-03 14:07:42 +0700

    some starter

---
+++ app/auth/login/Login_Form.tsx
@@ -0,0 +1,12 @@
+// @flow
+import * as React from 'react';
+
+type Props = {};
+
+export function _LoginForm(props: Props) {
+    return (
+        <div>
+            Register Form
+        </div>
+    );
+}
